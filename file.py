a1a71c72708894dcf0d806f5f30701a1fb8e3bf6
Sribhuvan-25 <sribhuvan.reddy@gmail.com>
2023-02-11 14:34:42 -0500

    Built network and generated melody

---
+++ file.py
@@ -0,0 +1,6 @@
+SONGS_PATH = "Data/erk"
+ACCEPTABLE_DURATIONS = [0.25, 0.5, 0.75, 1.0, 1.5, 2, 3, 4]
+SAVE = "Dataset"
+SINGLE_FILE_PATH = "misc/single_file"
+LENGTH = 64
+DICT_PATH = "misc/dict.json"
